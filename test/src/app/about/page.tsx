9eb0366d0d52c95015ede5de797eec2f6374da78
Sanskrati Agrawal <youremail@gmail.com>
2024-03-16 22:39:52 +0530

    nextjs routes

--- test/src/app/about/page.tsx
+++ test/src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata ={
+    title :" About title",
+    description: 'Inside About'
+}
 export default function Home (){
     return <h1>About page</h1>
 } 
--- test/src/app/products/[productsid]/page.tsx
+++ test/src/app/products/[productsid]/page.tsx
@@ -1,6 +1,15 @@
-export default function Home ({params}:{
-    params:{productsid: string}
-}){
+import { Metadata } from "next";
+
+type Props =  { params:{productsid: string}};
+
+export const generateMetadata = ({params}:Props):Metadata=>{
+    return {
+
+        title : `Product-${params.productsid}`
+    }
+}
+
+export default function Home ({params}:Props){
     return <>
         <h1>product1 - {params.productsid}</h1>
         <h1>product2</h1>
